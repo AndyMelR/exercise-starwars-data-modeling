619914a794950d48c36cbb44b4d88343e093bd3b
AndyMelR <ameleror@hotmail.es>
2023-04-05 16:10:51 +0000

    changed columns

--- src/models.py
+++ src/models.py
@@ -32,8 +32,6 @@ class Affiliation(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
     description = Column(String(700))
-    films_id = Column(Integer, ForeignKey('films.id'))
-    films = relationship(Films)
 
 class Planet(Base):
     __tablename__ = 'planet'
@@ -44,25 +42,17 @@ class Planet(Base):
     climate = Column(String(200))
     terrain = Column(String(50))
     history = Column(String(700))
-    films_id = Column(Integer, ForeignKey('films.id'))
-    films = relationship(Films)
-    affiliation_id = Column(Integer, ForeignKey('affiliation.id'))
-    affiliation = relationship(Affiliation)
 
 class Species(Base):
     __tablename__ = 'species'
     id = Column(Integer, primary_key=True)
     name = Column(String(300), nullable=False)
     description = Column(String(500))
-    films_id = Column(Integer, ForeignKey('films.id'))
-    films = relationship(Films)
-    affiliation_id = Column(Integer, ForeignKey('affiliation.id'))
-    affiliation = relationship(Affiliation)
 
 class Gender(Base):
     __tablename__ = 'gender'
     id= Column(Integer, primary_key=True)
-    gender = Column(String(50))
+    name = Column(String(50))
 
 class Characters(Base):
     __tablename__ = 'characters'
@@ -71,14 +61,13 @@ class Characters(Base):
     description = Column(String(500))
     dimensions = Column(Float)
     specie_id = Column(Integer, ForeignKey('species.id'))
-    species = relationship(Species)
+    character_specie = relationship(Species)
     gender_id = Column(Integer, ForeignKey('gender.id'))
-    gender = relationship(Gender)
-    films_id = Column(Integer, ForeignKey('films.id'))
-    films = relationship(Films)
+    character_gender = relationship(Gender)
     affiliation_id = Column(Integer, ForeignKey('affiliation.id'))
-    affiliation = relationship(Affiliation)
+    character_affiliation = relationship(Affiliation)
 
+    
 class Creatures(Base):
     __tablename__ ='creatures'
     id = Column(Integer, primary_key=True)
@@ -87,15 +76,6 @@ class Creatures(Base):
     dimensions = Column(Float)
     gender_id = Column(Integer, ForeignKey('gender.id'))
     gender = relationship(Gender)
-
-class Nature(Base):
-    __tablename__ = 'nature'
-    id = Column(Integer, primary_key=True)
-    specie_id = Column(Integer, ForeignKey('species.id'))
-    species = relationship(Species)
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-    planet = relationship(Planet)
-
 
 class Favorites(Base):
     __tablename__ = 'favorites'
@@ -110,6 +90,8 @@ class Favorites(Base):
     planet = relationship(Planet)
     characters_id = Column(Integer, ForeignKey('characters.id'))
     characters= relationship(Characters)
+    creature_id = Column(Integer, ForeignKey('creatures.id'))
+    creatures = relationship(Creatures)
 
 
     def to_dict(self):
